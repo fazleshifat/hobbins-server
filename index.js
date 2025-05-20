4d8ca3637af07680926c60358b1b00707ea6a542
fazleshifat <mailbox189.shifat@gmail.com>
2025-05-21 02:20:32 +0600

    added indivisual data for each group to access form backend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -56,6 +56,13 @@ async function run() {
 
         app.get('/groups', async (req, res) => {
             const result = await groupsCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/group/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await groupsCollection.findOne(query);
             res.send(result);
         })
 
